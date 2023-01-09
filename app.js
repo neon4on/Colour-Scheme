4534ef63a8dd56e7f35cf1534bb6fee4a6d614d8
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-01-10 00:10:10 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -12,11 +12,19 @@ function generateRandomColour(){
 function setRandomColors(){
     cols.forEach(col =>{
         const text = col.querySelector('h2')
-        const color = generateRandomColour()
+        const color = chroma.random()
+
         
         text.textContent = color
         col.style.background = color
+
+        setTextColor(text, color)
     })
+}
+
+function setTextColor(text, color){
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRandomColors()
