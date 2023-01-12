1e8038be96444146de60d3c0e8c13f1311812954
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-01-12 13:43:16 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cols = document.querySelectorAll('.col')
 
 document.addEventListener('keydown', (event) => {
+    event.preventDefault()
     if (event.code.toLocaleLowerCase() == 'space'){
         setRandomColors()
     }
@@ -10,7 +11,10 @@ document.addEventListener('click', (event) => {
     const type = event.target.dataset.type
 
     if (type == 'lock') {
-        console.log('perform lock')
+        const node = event.target.tagName.toLowerCase() == 'i' ? event.target : event.target.children[0]
+        
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
     }
 })
 
